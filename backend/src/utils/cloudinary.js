e62c92a01ebd73a7b2210137dc243cbab3c6231b
qasimnauman <m.qasimnauman@gmail.com>
2025-01-07 01:23:31 +0500

    Added the Cloudinary Utlity

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localfilepath) => {
+    try {
+        if (!localfilepath) return null
+        // Uploading file on Cloudindary
+        const response = await cloudinary.uploader.upload(localfilepath, {
+            resource_type: "auto"
+        })
+        // File has been uploaded successfully
+        console.log("File Uploaded Successfully on Cloudinary", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localfilepath) //Removes the locally saved temoprary file as the upload operation fails
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
