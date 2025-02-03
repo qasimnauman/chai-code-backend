e7d4d58dee5a3009f65b46421f1dd201aba122c6
qasimnauman <m.qasimnauman@gmail.com>
2025-02-04 01:19:59 +0500

    Enhance Cloudinary utility with delete functionality and update upload method to include user ID and directory

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,6 +1,7 @@
 import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 import path from "path";
+import { Apierror } from "./ApiError.js";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -8,16 +9,17 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const uploadOnCloudinary = async (localfilepath) => {
+const uploadOnCloudinary = async (localfilepath, userId, directory) => {
     try {
         if (!localfilepath) return null
         const filename = path.basename(localfilepath)
+        console.log(filename);
         // Uploading file on Cloudindary
         const response = await cloudinary.uploader.upload(localfilepath, {
             resource_type: "auto",
             use_filename: true,
-            public_id: `youtube/${filename}`,
-            folder: "youtube",
+            public_id: `${userId}-${filename}`,
+            folder: `youtube/${directory}`,
             overwrite: true,
             chunk_size: 6000000
         })
@@ -30,4 +32,30 @@ const uploadOnCloudinary = async (localfilepath) => {
     }
 }
 
-export { uploadOnCloudinary }
+const deletefromCloudinary = async (url) => {
+    try {
+        const splitURL = url.split("/");
+        console.log("Split URL:", splitURL);
+
+        let publicId = splitURL.slice(7).join("/");
+
+        publicId = publicId.replace(/\.(jpg|jpeg|png|webp|gif|mp4|mov|avi|mkv)$/, "");
+
+        console.log("Corrected Public ID:", publicId);
+
+        const fileExtension = url.split('.').pop().toLowerCase();
+        let resourceType = fileExtension === "mp4" || fileExtension === "mov" || fileExtension === "avi" || fileExtension === "mkv" ? "video" : "image";
+
+        const deleteResponse = await cloudinary.uploader.destroy(publicId, { resource_type: resourceType });
+
+        console.log("Delete Response:", deleteResponse);
+        return deleteResponse;
+    } catch (error) {
+        throw new Apierror(404, `No resource found for ${url} to delete`);
+    }
+};
+
+export {
+    uploadOnCloudinary,
+    deletefromCloudinary
+}
